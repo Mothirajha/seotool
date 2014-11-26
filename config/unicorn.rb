1194d820c24ef403256966cb5b2e0bb2d9a93f42
Mothirajha <rajha12@gmail.com>
2014-11-26 15:02:05 +0530

    removed unicorn_init.sh file and changes in mina deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,28 @@
-root = "/home/deployer/apps/seotool/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
-listen "/tmp/unicorn.seotool.sock"
-worker_processes 2
-timeout 30
+app_path = "/home/deployer/apps/seotool/current"
+ 
+worker_processes   1
+preload_app        true
+timeout            180
+listen             '127.0.0.1:80'
+user               'deployer'
+working_directory  app_path
+pid                "#{app_path}/tmp/pids/unicorn.pid"
+stderr_path        "log/unicorn.log"
+stdout_path        "log/unicorn.log"
+ 
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+ 
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+ 
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end
